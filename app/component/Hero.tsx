b5311e921628572f5989762bf252330c184c3947
Alex Gichau <gichaumburu@gmail.com>
2025-06-02 16:13:24 +0300

    Hero added

--- app/component/Hero.tsx
+++ app/component/Hero.tsx
@@ -1,9 +1,9 @@
 export default function Hero() {
     return (
         <section className="min-h-screen flex flex-col items-center justify-center text-center bg-gray-50">
-            <h1 className="text-5xl font-bold mb-4">Clara Njeri</h1>
+            <h1 className="text-5xl font-bold mb-4 text-gray-950">Clara Njeri</h1>
             <p className="text-xl text-gray-600">Virtual Assistant</p>
-            <button className="mt-8 px-6 bg-black text-white rounded-lg hover:bg-gray-800 transition">View Projects</button>
+            <button className="mt-8 px-6 py-3 bg-black text-white rounded-lg hover:bg-gray-800 transition">View Projects</button>
         </section>
     )
 }
