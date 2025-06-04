be1dc7fd0a6aa699097f2c90f06a19f20c74a165
Alex Gichau <gichaumburu@gmail.com>
2025-06-04 21:43:11 +0300

    project sections

--- app/component/Projects.tsx
+++ app/component/Projects.tsx
@@ -1,38 +1,32 @@
-// import { useEffect, useState } from "react";
-
-// interface Projects {
-//     id: number;
-//     title: string;
-//     desc: string
-// }
+import { FiActivity, FiBook, FiGlobe } from "react-icons/fi"
 
 const projects = [{
     id: 1,
     title: 'Portfolio',
+    icon: <FiBook />,
     desc: 'My Virtual Assistant'
 }, {
     id: 2,
     title: 'E-commerce',
+    icon: <FiActivity />,
+    desc: 'Online Store Management'
+}, {
+    id: 2,
+    title: 'Online Store',
+    icon: <FiGlobe />,
     desc: 'Online Store Management'
 
 }]
 
 export default function Projects() {
-    // const [projects, setProjects] = useState<Projects[]>([]);
-
-    // useEffect(() => {
-    //     // Fetch projects data from the API endpoint
-    //     fetch('/api/projects')
-    //         .then((res) => res.json())
-    //         .then(setProjects);
-        
-    // }, []);
 
     return (
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 p-8">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8 p-8 mx-auto max-w-7xl">
                 {projects.map((project) => (
                     <div key={project.id} className="border p-6 rounded-lg hover:shadow-lg transition">
-                        <h3>{project.title}</h3>
+                        <div className="flex flex-row justify-between items-center"><h3 className="bold text-2xl">{project.title}</h3>
+                        <p className="text-6xl">{project.icon}</p>
+                        </div>
                         <p>{project.desc}</p>
                     </div>
                 ))}
