18990c3262cf5488f3d4b5e857cdf21fdfd9cdbc
Alex Gichau <gichaumburu@gmail.com>
2025-06-06 15:15:03 +0300

    footer added

---
+++ app/component/Footer.tsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+    return (
+        <footer className="bg-gray-900 text-white py-6">
+            <div className="container mx-auto text-center">
+                <p className="text-sm mb-2">© 2025 Clara Njeri. All rights reserved.</p>
+                <p className="text-sm">Follow me on 
+                    <a href="https://twitter.com/ClaraNjeri" className="text-blue-400 hover:underline ml-1">Twitter</a>, 
+                    <a href="https://www.linkedin.com/in/clara-njeri/" className="text-blue-400 hover:underline ml-1">LinkedIn</a>, 
+                    <a href="https://github.com/ClaraNjeri" className="text-blue-400 hover:underline ml-1">GitHub</a>
+                </p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
