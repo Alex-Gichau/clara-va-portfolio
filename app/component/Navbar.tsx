6cbc9fa4a8cf2db43d6d7ad6312c3022b2003100
Alex Gichau <gichaumburu@gmail.com>
2025-06-09 00:01:50 +0300

    navbar corrected

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -8,14 +8,14 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ profileImage, name }) => {
     return (
-        <nav className="sticky top-0 z-50 mt-5 bg-opacity-40 backdrop-blur drop-shadow-amber-100 border-b border-gray-200 py-4 px-6 flex justify-between items-center shadow-2xl text-black mx-auto max-w-xl my-1.5 rounded-full">
+        <nav className="sticky top-0 z-50 mt-5 backdrop-blur border-b border-gray-200 py-4 px-6 flex justify-between items-center shadow-2xl text-black mx-auto max-w-xl my-1.5 rounded-full">
 
             <div className="flex items-center space-x-4">
                 <Image src={profileImage} alt="Profile" className="w-10 h-10 rounded-full" />
                 <span className="text-lg font-semibold">{name}</span>
             </div>
             <ul className="flex space-x-6">
-                <li><a href="#home" className="hover:text-greay-400">Home</a></li>
+                <li><a href="#home" className="hover:text-gray-400">Home</a></li>
                 <li><a href="#projects" className="hover:text-gray-400">Projects</a></li>
                 <li><a href="#contact" className="hover:text-gray-400">Contact</a></li>
             </ul>
