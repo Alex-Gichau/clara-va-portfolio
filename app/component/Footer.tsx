3e8ef747c83a91db6b9f36eeb0187b26d3b8971d
Alex Gichau <gichaumburu@gmail.com>
2025-06-13 20:08:52 +0300

    footer margin bottom added

--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -3,7 +3,7 @@ import { FiGithub, FiLinkedin, FiTwitter } from "react-icons/fi";
 
 export default function Footer() {
     return (
-        <footer className="bg-gray-900 text-white py-6 max-w-5xl mx-auto rounded-1xl">
+        <footer className="bg-gray-900 text-white py-6 max-w-5xl mx-auto rounded-1xl mb-10">
             <div className="container mx-auto text-center">
                 <h2 className="text-3xl font-bold mb-4 flex align-center justify-center"><FiCircle className="mr-2 text-sm mt-3 text-green"/>Let's Connect</h2>
                 <p className="text-lg mb-4">Interested in working together? I'd love to hear from you!</p>
